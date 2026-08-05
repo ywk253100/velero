be6b4d38f2e828045801096ad74224744cb26e52
samay43 <samayrbhat43@gmail.com>
2026-08-05 20:24:35 +0530

    gofmt pkg/cmd/cli/backup/create.go
    Signed-off-by: samay43 <samayrbhat43@gmail.com>

--- pkg/cmd/cli/backup/create.go
+++ pkg/cmd/cli/backup/create.go
@@ -25,8 +25,8 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 	kubeerrs "k8s.io/apimachinery/pkg/util/errors"
-	"k8s.io/client-go/tools/cache"
 	"k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/client-go/tools/cache"
 	kbclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	velerov1api "github.com/vmware-tanzu/velero/pkg/apis/velero/v1"
@@ -189,10 +189,10 @@ func (o *CreateOptions) Validate(c *cobra.Command, args []string, f client.Facto
 
 	// Ensure the backup name is a valid Kubernetes resource name
 	if o.FromSchedule == "" {
-    if errs := validation.IsDNS1123Subdomain(o.Name); len(errs) > 0 {
-        return fmt.Errorf("invalid backup name %q: %s", o.Name, strings.Join(errs, "; "))
-    }
-}
+		if errs := validation.IsDNS1123Subdomain(o.Name); len(errs) > 0 {
+			return fmt.Errorf("invalid backup name %q: %s", o.Name, strings.Join(errs, "; "))
+		}
+	}
 
 	errs := collections.ValidateNamespaceIncludesExcludes(o.IncludeNamespaces, o.ExcludeNamespaces)
 	if len(errs) > 0 {
